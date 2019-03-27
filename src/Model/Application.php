af7fe2ee8e0b523462ca35ab59fb1bac93d5cf4e
LupusVII <jln.thirion@gmail.com>
2019-03-27 11:27:05 +0100

    Fix namespaces

--- src/Model/Application.php
+++ src/Model/Application.php
@@ -1,14 +1,6 @@
 <?php
 
-/**
- * Prezioso Extension for Contao Open Source CMS
- *
- * Copyright (c) 2015-2018 Web ex Machina
- *
- * @author Web ex Machina <https://www.webexmachina.fr>
- */
-
-namespace Prezioso\Model;
+namespace WEM\JobOffersBundle\Model;
 
 use Contao\Model;
 
@@ -17,70 +9,76 @@ use Contao\Model;
  */
 class Application extends Model
 {
-	/**
-	 * Table name
-	 * @var string
-	 */
-	protected static $strTable = 'tl_pzl_job_application';
+    /**
+     * Table name
+     * @var string
+     */
+    protected static $strTable = 'tl_pzl_job_application';
 
-	/**
-	 * Find items, depends on the arguments
-	 * @param Array
-	 * @param Int
-	 * @param Int
-	 * @param Array
-	 * @return Collection
-	 */
-	public static function findItems($arrConfig = array(), $intLimit = 0, $intOffset = 0, $arrOptions = array())
-	{
-		$t = static::$strTable;
-		$arrColumns = static::formatColumns($arrConfig);
-			
-		if($intLimit > 0)
-			$arrOptions['limit'] = $intLimit;
+    /**
+     * Find items, depends on the arguments
+     * @param Array
+     * @param Int
+     * @param Int
+     * @param Array
+     * @return Collection
+     */
+    public static function findItems($arrConfig = array(), $intLimit = 0, $intOffset = 0, $arrOptions = array())
+    {
+        $t = static::$strTable;
+        $arrColumns = static::formatColumns($arrConfig);
+            
+        if ($intLimit > 0) {
+            $arrOptions['limit'] = $intLimit;
+        }
 
-		if($intOffset > 0)
-			$arrOptions['offset'] = $intOffset;
+        if ($intOffset > 0) {
+            $arrOptions['offset'] = $intOffset;
+        }
 
-		if(!isset($arrOptions['order']))
-			$arrOptions['order'] = "$t.tstamp DESC";
+        if (!isset($arrOptions['order'])) {
+            $arrOptions['order'] = "$t.tstamp DESC";
+        }
 
-		if(empty($arrColumns))
-			return static::findAll($arrOptions);
-		else
-			return static::findBy($arrColumns, null, $arrOptions);
-	}
+        if (empty($arrColumns)) {
+            return static::findAll($arrOptions);
+        } else {
+            return static::findBy($arrColumns, null, $arrOptions);
+        }
+    }
 
-	/**
-	 * Count items, depends on the arguments
-	 * @param Array
-	 * @param Array
-	 * @return Integer
-	 */
-	public static function countItems($arrConfig = array(), $arrOptions = array())
-	{
-		$t = static::$strTable;
-		$arrColumns = static::formatColumns($arrConfig);
+    /**
+     * Count items, depends on the arguments
+     * @param Array
+     * @param Array
+     * @return Integer
+     */
+    public static function countItems($arrConfig = array(), $arrOptions = array())
+    {
+        $t = static::$strTable;
+        $arrColumns = static::formatColumns($arrConfig);
 
-		if(empty($arrColumns))
-			return static::countAll($arrOptions);
-		else
-			return static::countBy($arrColumns, null, $arrOptions);
-	}
+        if (empty($arrColumns)) {
+            return static::countAll($arrOptions);
+        } else {
+            return static::countBy($arrColumns, null, $arrOptions);
+        }
+    }
 
-	/**
-	 * Format ItemModel columns
-	 * @param  [Array] $arrConfig [Configuration to format]
-	 * @return [Array]            [The Model columns]
-	 */
-	public static function formatColumns($arrConfig)
-	{
-		$t = static::$strTable;
-		$arrColumns = array();
+    /**
+     * Format ItemModel columns
+     * @param  [Array] $arrConfig [Configuration to format]
+     * @return [Array]            [The Model columns]
+     */
+    public static function formatColumns($arrConfig)
+    {
+        $t = static::$strTable;
+        $arrColumns = array();
 
-		if($arrConfig["not"])
-			$arrColumns[] = $arrConfig["not"];
+        if ($arrConfig["not"]) {
+            $arrColumns[] = $arrConfig["not"];
+        }
 
-		return $arrColumns;
-	}
+        return $arrColumns;
+    }
 }
--- src/Module/ModuleJobsList.php
+++ src/Module/ModuleJobsList.php
@@ -1,14 +1,6 @@
 <?php
 
-/**
- * Prezioso Extension for Contao Open Source CMS
- *
- * Copyright (c) 2015-2018 Web ex Machina
- *
- * @author Web ex Machina <https://www.webexmachina.fr>
- */
-
-namespace Prezioso\Module;
+namespace WEM\JobOffersBundle\Module;
 
 use Contao\CoreBundle\Exception\PageNotFoundException;
 use Contao\Input;
@@ -24,204 +16,216 @@ use Prezioso\Model\Job as JobModel;
 class ModuleJobsList extends \Module
 {
 
-	/**
-	 * Template
-	 * @var string
-	 */
-	protected $strTemplate = 'mod_jobslist';
+    /**
+     * Template
+     * @var string
+     */
+    protected $strTemplate = 'mod_jobslist';
 
-	/**
-	 * Display a wildcard in the back end
-	 *
-	 * @return string
-	 */
-	public function generate()
-	{
-		if (TL_MODE == 'BE'){
-			$objTemplate = new \BackendTemplate('be_wildcard');
-			$objTemplate->wildcard = '### ' . Utf8::strtoupper($GLOBALS['TL_LANG']['FMD']['jobslist'][0]) . ' ###';
-			$objTemplate->title = $this->headline;
-			$objTemplate->id = $this->id;
-			$objTemplate->link = $this->name;
-			$objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
+    /**
+     * Display a wildcard in the back end
+     *
+     * @return string
+     */
+    public function generate()
+    {
+        if (TL_MODE == 'BE') {
+            $objTemplate = new \BackendTemplate('be_wildcard');
+            $objTemplate->wildcard = '### ' . Utf8::strtoupper($GLOBALS['TL_LANG']['FMD']['jobslist'][0]) . ' ###';
+            $objTemplate->title = $this->headline;
+            $objTemplate->id = $this->id;
+            $objTemplate->link = $this->name;
+            $objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
 
-			return $objTemplate->parse();
-		}
+            return $objTemplate->parse();
+        }
 
-		return parent::generate();
-	}
+        return parent::generate();
+    }
 
-	/**
-	 * Generate the module
-	 */
-	protected function compile()
-	{
-		$strForm = $this->getForm(1);
+    /**
+     * Generate the module
+     */
+    protected function compile()
+    {
+        $strForm = $this->getForm(1);
 
-		if(Input::get('apply') && !Input::post('FORM_SUBMIT')){
-			$objJob = JobModel::findByPk(Input::get('apply'));
+        if (Input::get('apply') && !Input::post('FORM_SUBMIT')) {
+            $objJob = JobModel::findByPk(Input::get('apply'));
 
-			$objTemplate = new \FrontendTemplate('job_apply');
-			$objTemplate->id =  $objJob->id;
-			$objTemplate->code =  $objJob->code;
-			$objTemplate->title =  $objJob->title;
-			$objTemplate->recipient =  $objJob->recipient ?: $GLOBALS['TL_ADMIN_EMAIL'];
-			$objTemplate->time =  time();
-			$objTemplate->token =  \RequestToken::get();
-			$objTemplate->form = $strForm;
-			
-			echo $objTemplate->parse();
-			die;
-		}
+            $objTemplate = new \FrontendTemplate('job_apply');
+            $objTemplate->id =  $objJob->id;
+            $objTemplate->code =  $objJob->code;
+            $objTemplate->title =  $objJob->title;
+            $objTemplate->recipient =  $objJob->recipient ?: $GLOBALS['TL_ADMIN_EMAIL'];
+            $objTemplate->time =  time();
+            $objTemplate->token =  \RequestToken::get();
+            $objTemplate->form = $strForm;
+            
+            echo $objTemplate->parse();
+            die;
+        }
 
-		global $objPage;
-		$limit = null;
-		$offset = (int) $this->skipFirst;
+        global $objPage;
+        $limit = null;
+        $offset = (int) $this->skipFirst;
 
-		// Maximum number of items
-		if ($this->numberOfItems > 0)
-			$limit = $this->numberOfItems;
+        // Maximum number of items
+        if ($this->numberOfItems > 0) {
+            $limit = $this->numberOfItems;
+        }
 
-		$arrConfig = ["published"=>1];
-		$this->Template->articles = array();
-		$this->Template->empty = $GLOBALS['TL_LANG']['MSC']['emptyList'];
+        $arrConfig = ["published"=>1];
+        $this->Template->articles = array();
+        $this->Template->empty = $GLOBALS['TL_LANG']['MSC']['emptyList'];
 
-		// Get the available filters
-		$objJobFilters = JobModel::findItems(["published"=>1]);
-		if($objJobFilters && 0 < $objJobFilters->count()){
-			$arrJobFilters = [];
-			$arrLocationFilters = [];
-			while($objJobFilters->next()){
-				if(!in_array($objJobFilters->title, $arrJobFilters))
-					$arrJobFilters[] = $objJobFilters->title;
+        // Get the available filters
+        $objJobFilters = JobModel::findItems(["published"=>1]);
+        if ($objJobFilters && 0 < $objJobFilters->count()) {
+            $arrJobFilters = [];
+            $arrLocationFilters = [];
+            while ($objJobFilters->next()) {
+                if (!in_array($objJobFilters->title, $arrJobFilters)) {
+                    $arrJobFilters[] = $objJobFilters->title;
+                }
 
-				if(!in_array($objJobFilters->location, $arrLocationFilters))
-					$arrLocationFilters[] = $objJobFilters->location;
-			}
-			$this->Template->jobFilters = $arrJobFilters;
-			$this->Template->locationFilters = $arrLocationFilters;
-		}
+                if (!in_array($objJobFilters->location, $arrLocationFilters)) {
+                    $arrLocationFilters[] = $objJobFilters->location;
+                }
+            }
+            $this->Template->jobFilters = $arrJobFilters;
+            $this->Template->locationFilters = $arrLocationFilters;
+        }
 
-		// Add job to the config if there is a filter
-		if(\Input::get('job'))
-			$arrConfig['title'] = \Input::get('job');
-		
-		// Add area to the config if there is a filter
-		if(\Input::get('location'))
-			$arrConfig['location'] = \Input::get('location');
+        // Add job to the config if there is a filter
+        if (\Input::get('job')) {
+            $arrConfig['title'] = \Input::get('job');
+        }
+        
+        // Add area to the config if there is a filter
+        if (\Input::get('location')) {
+            $arrConfig['location'] = \Input::get('location');
+        }
 
-		// Get the total number of items
-		$intTotal = JobModel::countItems($arrConfig);
+        // Get the total number of items
+        $intTotal = JobModel::countItems($arrConfig);
 
-		if ($intTotal < 1)
-			return;
+        if ($intTotal < 1) {
+            return;
+        }
 
-		$total = $intTotal - $offset;
+        $total = $intTotal - $offset;
 
-		// Split the results
-		if ($this->perPage > 0 && (!isset($limit) || $this->numberOfItems > $this->perPage)){
-			// Adjust the overall limit
-			if (isset($limit))
-				$total = min($limit, $total);
+        // Split the results
+        if ($this->perPage > 0 && (!isset($limit) || $this->numberOfItems > $this->perPage)) {
+            // Adjust the overall limit
+            if (isset($limit)) {
+                $total = min($limit, $total);
+            }
 
-			// Get the current page
-			$id = 'page_n' . $this->id;
-			$page = \Input::get($id) ?? 1;
+            // Get the current page
+            $id = 'page_n' . $this->id;
+            $page = \Input::get($id) ?? 1;
 
-			// Do not index or cache the page if the page number is outside the range
-			if ($page < 1 || $page > max(ceil($total/$this->perPage), 1))
-				throw new PageNotFoundException('Page not found: ' . \Environment::get('uri'));
+            // Do not index or cache the page if the page number is outside the range
+            if ($page < 1 || $page > max(ceil($total/$this->perPage), 1)) {
+                throw new PageNotFoundException('Page not found: ' . \Environment::get('uri'));
+            }
 
-			// Set limit and offset
-			$limit = $this->perPage;
-			$offset += (max($page, 1) - 1) * $this->perPage;
-			$skip = (int) $this->skipFirst;
+            // Set limit and offset
+            $limit = $this->perPage;
+            $offset += (max($page, 1) - 1) * $this->perPage;
+            $skip = (int) $this->skipFirst;
 
-			// Overall limit
-			if ($offset + $limit > $total + $skip)
-				$limit = $total + $skip - $offset;
+            // Overall limit
+            if ($offset + $limit > $total + $skip) {
+                $limit = $total + $skip - $offset;
+            }
 
-			// Add the pagination menu
-			$objPagination = new \Pagination($total, $this->perPage, \Config::get('maxPaginationLinks'), $id);
-			$this->Template->pagination = $objPagination->generate("\n  ");
-		}
+            // Add the pagination menu
+            $objPagination = new \Pagination($total, $this->perPage, \Config::get('maxPaginationLinks'), $id);
+            $this->Template->pagination = $objPagination->generate("\n  ");
+        }
 
-		$objArticles = JobModel::findItems($arrConfig, ($limit ?: 0), ($offset ?: 0));
+        $objArticles = JobModel::findItems($arrConfig, ($limit ?: 0), ($offset ?: 0));
 
-		// Add the articles
-		if ($objArticles !== null)
-			$this->Template->articles = $this->parseArticles($objArticles);
+        // Add the articles
+        if ($objArticles !== null) {
+            $this->Template->articles = $this->parseArticles($objArticles);
+        }
 
-		// Load JS
-		$objCombiner = new \Combiner();
-		$objCombiner->add("system/modules/prezioso/assets/js/mod_joblist.js", time());
-		$GLOBALS["TL_JQUERY"][] = '<script src="https://www.google.com/recaptcha/api.js"></script>';
-		$GLOBALS["TL_JQUERY"][] = sprintf('<script src="%s"></script>', $objCombiner->getCombinedFile());
-	}
+        // Load JS
+        $objCombiner = new \Combiner();
+        $objCombiner->add("system/modules/prezioso/assets/js/mod_joblist.js", time());
+        $GLOBALS["TL_JQUERY"][] = '<script src="https://www.google.com/recaptcha/api.js"></script>';
+        $GLOBALS["TL_JQUERY"][] = sprintf('<script src="%s"></script>', $objCombiner->getCombinedFile());
+    }
 
-	/**
-	 * Parse one or more items and return them as array
-	 *
-	 * @param Model\Collection $objArticles
-	 * @param boolean          $blnAddArchive
-	 *
-	 * @return array
-	 */
-	protected function parseArticles($objArticles, $blnAddArchive=false)
-	{
-		$limit = $objArticles->count();
+    /**
+     * Parse one or more items and return them as array
+     *
+     * @param Model\Collection $objArticles
+     * @param boolean          $blnAddArchive
+     *
+     * @return array
+     */
+    protected function parseArticles($objArticles, $blnAddArchive = false)
+    {
+        $limit = $objArticles->count();
 
-		if ($limit < 1)
-			return array();
+        if ($limit < 1) {
+            return array();
+        }
 
-		$count = 0;
-		$arrArticles = array();
+        $count = 0;
+        $arrArticles = array();
 
-		while ($objArticles->next()){
-			/** @var NewsModel $objArticle */
-			$objArticle = $objArticles->current();
+        while ($objArticles->next()) {
+            /** @var NewsModel $objArticle */
+            $objArticle = $objArticles->current();
 
-			$arrArticles[] = $this->parseArticle($objArticle, $blnAddArchive, ((++$count == 1) ? ' first' : '') . (($count == $limit) ? ' last' : '') . ((($count % 2) == 0) ? ' odd' : ' even'), $count);
-		}
+            $arrArticles[] = $this->parseArticle($objArticle, $blnAddArchive, ((++$count == 1) ? ' first' : '') . (($count == $limit) ? ' last' : '') . ((($count % 2) == 0) ? ' odd' : ' even'), $count);
+        }
 
-		return $arrArticles;
-	}
+        return $arrArticles;
+    }
 
-	/**
-	 * Parse an item and return it as string
-	 *
-	 * @param NewsModel $objArticle
-	 * @param boolean   $blnAddArchive
-	 * @param string    $strClass
-	 * @param integer   $intCount
-	 *
-	 * @return string
-	 */
-	protected function parseArticle($objArticle, $blnAddArchive=false, $strClass='', $intCount=0)
-	{
-		$objTemplate = new \FrontendTemplate($this->job_template);
-		$objTemplate->setData($objArticle->row());
+    /**
+     * Parse an item and return it as string
+     *
+     * @param NewsModel $objArticle
+     * @param boolean   $blnAddArchive
+     * @param string    $strClass
+     * @param integer   $intCount
+     *
+     * @return string
+     */
+    protected function parseArticle($objArticle, $blnAddArchive = false, $strClass = '', $intCount = 0)
+    {
+        $objTemplate = new \FrontendTemplate($this->job_template);
+        $objTemplate->setData($objArticle->row());
 
-		if ($objArticle->cssClass != '')
-			$strClass = ' ' . $objArticle->cssClass . $strClass;
+        if ($objArticle->cssClass != '') {
+            $strClass = ' ' . $objArticle->cssClass . $strClass;
+        }
 
-		$objTemplate->class = $strClass;
-		$objTemplate->count = $intCount; // see #5708
+        $objTemplate->class = $strClass;
+        $objTemplate->count = $intCount; // see #5708
 
-		// Add the meta information
-		$objTemplate->date = $arrMeta['createdAt'];
-		$objTemplate->timestamp = $objArticle->date;
-		$objTemplate->datetime = date('Y-m-d\TH:i:sP', $objArticle->date);
+        // Add the meta information
+        $objTemplate->date = $arrMeta['createdAt'];
+        $objTemplate->timestamp = $objArticle->date;
+        $objTemplate->datetime = date('Y-m-d\TH:i:sP', $objArticle->date);
 
-		$objTemplate->applyUrl = $this->addToUrl("apply=".$objArticle->id, true, ["job"]);
+        $objTemplate->applyUrl = $this->addToUrl("apply=".$objArticle->id, true, ["job"]);
 
-		// Tag the response
-		if (\System::getContainer()->has('fos_http_cache.http.symfony_response_tagger')){
-			/** @var ResponseTagger $responseTagger */
-			$responseTagger = \System::getContainer()->get('fos_http_cache.http.symfony_response_tagger');
-			$responseTagger->addTags(array('contao.db.tl_pzl_job.' . $objArticle->id));
-		}
+        // Tag the response
+        if (\System::getContainer()->has('fos_http_cache.http.symfony_response_tagger')) {
+            /** @var ResponseTagger $responseTagger */
+            $responseTagger = \System::getContainer()->get('fos_http_cache.http.symfony_response_tagger');
+            $responseTagger->addTags(array('contao.db.tl_pzl_job.' . $objArticle->id));
+        }
 
-		return $objTemplate->parse();
-	}
+        return $objTemplate->parse();
+    }
 }
