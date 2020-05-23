9a684270e1206a36186f45776e2293bec6bcbb2b
LupusVII <jln.thirion@gmail.com>
2020-05-23 12:22:19 +0200

    Move applicant files into subfolders in order to not overload one folder with too many files

--- src/Hooks/StoreFormDataHook.php
+++ src/Hooks/StoreFormDataHook.php
@@ -21,14 +21,45 @@ class StoreFormDataHook
         try {
             if ('job-offer-application' === $objForm->formID) {
                 // Unset fields who are not in tl_wem_job_application table
+                $strCode = $arrSet['code'];
                 unset($arrSet['recipient'], $arrSet['code'], $arrSet['title']);
 
                 // Convert files path into uuid
                 if ($arrSet['cv'] && $objFile = \FilesModel::findOneByPath($arrSet['cv'])) {
                     $arrSet['cv'] = $objFile->uuid;
+
+                    // Move file into a subfolder with a clearer name
+                    // Rule: {form_folder}/{job_code}/{cv_lastname_firtname}
+                    $strNewName = sprintf(
+                        '%s/cv_%s_%s_%s.%s',
+                        $strCode,
+                        \StringUtil::generateAlias($arrSet['lastname']),
+                        \StringUtil::generateAlias($arrSet['firstname']),
+                        date('Y-m-d_H-i'),
+                        $objFile->extension
+                    );
+                    $strFilename = str_replace($objFile->name, $strNewName, $objFile->path);
+
+                    $objFile = new \File($objFile->path);
+                    $objFile->renameTo($strFilename);
                 }
                 if ($arrSet['applicationLetter'] && $objFile = \FilesModel::findOneByPath($arrSet['applicationLetter'])) {
                     $arrSet['applicationLetter'] = $objFile->uuid;
+
+                    // Move file into a subfolder with a clearer name
+                    // Rule: {form_folder}/{job_code}/{cv_lastname_firtname}
+                    $strNewName = sprintf(
+                        '%s/al_%s_%s_%s.%s',
+                        $strCode,
+                        \StringUtil::generateAlias($arrSet['lastname']),
+                        \StringUtil::generateAlias($arrSet['firstname']),
+                        date('Y-m-d_H-i'),
+                        $objFile->extension
+                    );
+                    $strFilename = str_replace($objFile->name, $strNewName, $objFile->path);
+
+                    $objFile = new \File($objFile->path);
+                    $objFile->renameTo($strFilename);
                 }
 
                 // Clean the session
