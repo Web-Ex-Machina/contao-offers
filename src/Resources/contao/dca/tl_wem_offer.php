32c3b8006582ca5a650fd1c0889c91fa685782e7
LupusVII <jln.thirion@ik.me>
2026-02-24 18:07:02 +0100

    Improve DCA + add translations

--- src/Resources/contao/dca/tl_wem_offer.php
+++ src/Resources/contao/dca/tl_wem_offer.php
@@ -167,30 +167,35 @@ $GLOBALS['TL_DCA']['tl_wem_offer'] = [
             'sql' => 'mediumtext NULL',
         ],
         'addImage' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['addImage'],
             'exclude' => true,
             'inputType' => 'checkbox',
             'eval' => ['submitOnChange' => true],
             'sql' => "char(1) NOT NULL default ''"
         ],
         'overwriteMeta' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['overwriteMeta'],
             'exclude' => true,
             'inputType' => 'checkbox',
             'eval' => ['submitOnChange' => true, 'tl_class' => 'w50 clr'],
             'sql' => "char(1) NOT NULL default ''"
         ],
         'singleSRC' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['singleSRC'],
             'exclude' => true,
             'inputType' => 'fileTree',
             'eval' => ['fieldType' => 'radio', 'filesOnly' => true, 'extensions' => '%contao.image.valid_extensions%', 'mandatory' => true],
             'sql' => "binary(16) NULL"
         ],
         'alt' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['alt'],
             'exclude' => true, 'search' => true,
             'inputType' => 'text',
             'eval' => ['maxlength' => 255, 'tl_class' => 'w50'],
             'sql' => "varchar(255) NOT NULL default ''"
         ],
         'imageTitle' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['imageTitle'],
             'exclude' => true,
             'search' => true,
             'inputType' => 'text',
@@ -198,6 +203,7 @@ $GLOBALS['TL_DCA']['tl_wem_offer'] = [
             'sql' => "varchar(255) NOT NULL default ''"
         ],
         'size' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['size'],
             'exclude' => true,
             'inputType' => 'imageSize',
             'reference' => &$GLOBALS['TL_LANG']['MSC'],
@@ -206,6 +212,7 @@ $GLOBALS['TL_DCA']['tl_wem_offer'] = [
             'sql' => "varchar(64) NOT NULL default ''"
         ],
         'imagemargin' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['imagemargin'],
             'exclude' => true,
             'inputType' => 'trbl',
             'options' => ['px', '%', 'em', 'rem'],
@@ -213,6 +220,7 @@ $GLOBALS['TL_DCA']['tl_wem_offer'] = [
             'sql' => "varchar(128) NOT NULL default ''"
         ],
         'imageUrl' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['imageUrl'],
             'exclude' => true,
             'search' => true,
             'inputType' => 'text',
@@ -220,12 +228,14 @@ $GLOBALS['TL_DCA']['tl_wem_offer'] = [
             'sql' => "varchar(2048) NOT NULL default ''"
         ],
         'fullsize' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['fullsize'],
             'exclude' => true,
             'inputType' => 'checkbox',
             'eval' => ['tl_class' => 'w50 m12'],
             'sql' => "char(1) NOT NULL default ''"
         ],
         'caption' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['caption'],
             'exclude' => true,
             'search' => true,
             'inputType' => 'text',
@@ -233,6 +243,7 @@ $GLOBALS['TL_DCA']['tl_wem_offer'] = [
             'sql' => "varchar(255) NOT NULL default ''"
         ],
         'floating' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['floating'],
             'exclude' => true,
             'inputType' => 'radioTable',
             'options' => ['above', 'left', 'right', 'below'],
@@ -252,7 +263,7 @@ $GLOBALS['TL_DCA']['tl_wem_offer'] = [
         'start' => [
             'exclude' => true,
             'inputType' => 'text',
-            'eval' => ['rgxp' => 'datim', 'datepicker' => true, 'tl_class' => 'w50 wizard'],
+            'eval' => ['rgxp' => 'datim', 'datepicker' => true, 'tl_class' => 'w50 clr wizard'],
             'sql' => "varchar(10) NOT NULL default ''",
         ],
         'stop' => [
